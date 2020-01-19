d6d642c49ba53c5ed242ed33a222ac10c1c196ab
atte-h <33579017+atte-h@users.noreply.github.com>
2020-01-19 23:52:29 +0200

    file write modification

--- main.py
+++ main.py
@@ -9,4 +9,14 @@ from Fibo import Fibo
 lengthOfSeries = int(input("Enter length of the series: "))
 startingNumber = int(input("Enter starting number: "))
 
-print(Fibo.fib(lengthOfSeries,startingNumber))
+res = Fibo.fib(lengthOfSeries,startingNumber)
+print(res)
+
+f = open("resultFile.txt", "a")
+i=0
+while i<len(res):
+  line = str(res[i])
+  f.write(str(line))
+  f.write("\n")
+  i += 1
+f.close()
